97369ff1dad9cb0e99dd6c6cf9e5ecfd6fe8515a
barak94 <barakza94@gmail.com>
2022-07-05 16:38:52 +0300

    create get req for api2

--- server/server.js
+++ server/server.js
@@ -4,13 +4,34 @@ const app = express();
 
 
 app.get("/hello", (req, res) => {
-    res.status(200).send("hello");
+  res.status(200).send("hello");
+})
+
+
+app.get("/distance", (req, res) => {
+
+  if (!req.query.source || !req.query.destination) {
+    return res.send({
+      error: "You must to provide a search term"
+    })
+  }
+
+
+  res.send({
+    source: req.query.source,
+    destination: req.query.destination
+  })
+
+})
+
+app.post("/distance", (req, res) => {
+
 })
 
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, ()=>{
+app.listen(port, () => {
 
   console.log("Listening on port :" + port);
 
