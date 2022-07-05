1564bd8093b51ae1bfb7611c3c292b6133e6b069
barak94 <barakza94@gmail.com>
2022-07-05 13:33:05 +0300

    created a get request for distance

--- server/server.js
+++ server/server.js
@@ -7,11 +7,33 @@ app.get("/hello", (req, res) => {
     res.status(200).send("hello");
 })
 
+app.get("/distance", (req, res) => {
+
+    if (!req.query.source || !req.query.destination) {
+        return res.send({
+            error: "You must to provide a source and destination"
+        })
+    }
+
+    res.send({
+        source:req.query.source,
+        destination:req.query.destination 
+    });
+
+})
+
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, ()=>{
+app.listen(port, () => {
 
-  console.log("Listening on port :" + port);
+    console.log("Listening on port :" + port);
 
 });
+
+
+// const arr = [{a:[1, 2], b:1, c:2}, {a:[3, 4], b:1, c:2 }, { a:[1, 2], b:1, c:2}];
+
+// const names = arr.filter((name) => {
+//     a.find()
+// })
